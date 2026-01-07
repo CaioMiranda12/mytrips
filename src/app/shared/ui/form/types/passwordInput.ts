793a09146bdd98c5d06b41808f4da3593effcc3b
CaioMiranda12 <caioclm04@gmail.com>
2026-01-07 17:05:47 -0300

    feat: create password input type

---
+++ src/app/shared/ui/form/types/passwordInput.ts
@@ -0,0 +1,9 @@
+import { IRHFProps } from "./types"
+
+export interface IIPasswordInputProps extends IRHFProps {
+  nameInput: string
+  nameLabelInput: string
+  namePlaceholderInput: string;
+  Icon: React.ReactNode;
+}
+
