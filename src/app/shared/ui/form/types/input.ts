ee2e9bdecb32fbb6b068a582b1224b76e6ec027a
CaioMiranda12 <caioclm04@gmail.com>
2026-01-07 17:05:39 -0300

    feat: create input type

---
+++ src/app/shared/ui/form/types/input.ts
@@ -0,0 +1,10 @@
+import { IRHFProps } from "./types"
+
+export interface IInputProps extends IRHFProps {
+  nameInput: string
+  nameLabelInput: string
+  typeInput: string
+  namePlaceholderInput: string;
+  Icon: React.ReactNode;
+}
+
