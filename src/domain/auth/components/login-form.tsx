39549d32770eaf498290e62a3652e1666b6e196e
CaioMiranda12 <caioclm04@gmail.com>
2026-01-08 11:15:20 -0300

    feat: add login-form

---
+++ src/domain/auth/components/login-form.tsx
@@ -0,0 +1,83 @@
+'use client'
+
+import { Lock, Mail } from "lucide-react"
+import { useLoginForm } from "../hooks/useLoginForm"
+import { LoginSchema, LoginSchemaType } from "../schemas/loginSchema"
+import { Input } from "@/app/shared/ui/form/input"
+import { PasswordInput } from "@/app/shared/ui/form/passwordInput"
+import { useAuthContext } from "@/app/shared/providers/AuthProvider"
+import { useRouter } from "next/navigation"
+import { useState } from "react"
+import { toast } from "react-toastify"
+
+export function LoginForm() {
+  const { signIn } = useAuthContext()
+  const router = useRouter();
+
+  const [errorMessage, setErrorMessage] = useState<string | null>(null)
+
+  const {
+    register,
+    handleSubmit,
+    control,
+    reset,
+    errors,
+    clearErrors
+  } = useLoginForm(LoginSchema)
+
+  const onSubmit = async (formData: LoginSchemaType) => {
+    const { email, password } = formData;
+
+    try {
+      await signIn(email, password)
+      router.push('/dashboard')
+
+    } catch (error) {
+      if (error instanceof Error) {
+        setErrorMessage(error.message)
+      } else {
+        setErrorMessage('Erro ao fazer login')
+      }
+
+      toast.error(errorMessage);
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className="space-y-5" noValidate>
+      <Input
+        name="email"
+        control={control}
+        errors={errors}
+        nameInput="Email"
+        nameLabelInput="email"
+        namePlaceholderInput="seu@email.com"
+        typeInput="text"
+        Icon={<Mail />}
+      />
+
+      <PasswordInput
+        name="password"
+        control={control}
+        errors={errors}
+        nameInput="Senha"
+        nameLabelInput="password"
+        namePlaceholderInput="••••••••"
+        Icon={<Lock />}
+      />
+
+      <div className="text-right">
+        <button type="button" className="text-sm text-cyan-600 hover:text-cyan-700 font-medium transition-colors">
+          Esqueci minha senha
+        </button>
+      </div>
+
+      <button
+        type="submit"
+        className="w-full py-3 bg-linear-to-r from-cyan-500 to-teal-500 text-white font-semibold rounded-xl hover:shadow-lg hover:scale-[1.02] transition-all duration-300"
+      >
+        Entrar
+      </button>
+    </form>
+  )
+}
