ded6457eef7b0e02770b8572ec617855029936fa
CaioMiranda12 <caioclm04@gmail.com>
2026-01-08 13:49:51 -0300

    fix: fix input icon using on password Input component

--- src/app/shared/ui/form/passwordInput.tsx
+++ src/app/shared/ui/form/passwordInput.tsx
@@ -24,32 +24,12 @@ export function PasswordInput({
       name={name}
       control={control}
       render={({ field }) => (
-        // <div>
-        //   <label htmlFor={nameLabelInput} className="block text-sm font-medium text-gray-700 mb-2">
-        //     {nameInput}
-        //   </label>
-        //   <div className="relative">
-        //     <Lock className="absolute left-3 top-1/2 -translate-y-1/2 w-5 h-5 text-gray-400" />
-        //     <input
-        //       {...field}
-        //       id={nameLabelInput}
-        //       name={nameLabelInput}
-        //       type={typeInput}
-        //       placeholder={namePlaceholderInput}
-        //       className="w-full pl-11 pr-4 py-3 border border-gray-300 rounded-xl focus:ring-2 focus:ring-cyan-500 focus:border-transparent outline-none transition-all"
-        //       required
-        //     />
-        //   </div>
-
-        //   <ErrorMessage message={errors?.[name]?.message} />
-        // </div>
-
         <div>
           <label htmlFor={nameLabelInput} className="block text-sm font-medium text-gray-700 mb-2">
             {nameInput}
           </label>
           <div className="relative">
-            <Lock className="absolute left-3 top-1/2 -translate-y-1/2 w-5 h-5 text-gray-400" />
+            <Icon className="absolute left-3 top-1/2 -translate-y-1/2 w-5 h-5 text-gray-400" />
             <input
               {...field}
               id={nameLabelInput}
