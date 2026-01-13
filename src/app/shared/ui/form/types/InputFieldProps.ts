961ef7debd9e2adada0bb56916a84343985057da
CaioMiranda12 <caioclm04@gmail.com>
2026-01-13 12:28:02 -0300

    feat: create InputFieldProps

---
+++ src/app/shared/ui/form/types/InputFieldProps.ts
@@ -0,0 +1,17 @@
+import { Control, FieldErrors } from "react-hook-form";
+import { ComponentType } from "react";
+import { IRHFProps } from "./types";
+import { LucideIcon } from "lucide-react";
+import { InputColorVariant } from "../styles/inputVariants";
+
+export interface InputFieldProps extends IRHFProps {
+  // name: string;
+  // control: Control<any>;
+  // errors: FieldErrors<any>;
+  label: string;
+  required?: boolean;
+  placeholder?: string;
+  typeInput?: "text" | "email" | "password" | "number";
+  Icon?: LucideIcon;
+  color?: InputColorVariant;
+}
